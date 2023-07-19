21c481dbee4690b12b20cb9a1fa620dbbac86446
Karolis <karolisdarbinis2@gmail.com>
2023-07-19 11:32:45 +0300

    Arrays

--- masyvai1_hm.php
+++ masyvai1_hm.php
@@ -79,15 +79,110 @@
 
 //d
 
-$arr = range(0, 29);
-$sum = 0;
+// $arr = range(0, 29);
+// $sum = 0;
 
-foreach($arr as $key => $value){
-    $value = rand(5,25);
-    print_r(" $key ---> $value |");
-    $sum .= $value - $key . ' ';
+// foreach($arr as $key => $value){
+//     $value = rand(5,25);
+//     print_r(" $key ---> $value |");
+//     $sum .= $value - $key . ' ';
 
+// }
+// echo '<br>';
+// echo '<br>';
+// echo $sum;
+
+//f
+
+// $arr = range(0, 29);
+// $masyvas1 = [];
+// $masyvas2 = [];
+
+// foreach($arr as $key => $value){
+//     $value = rand(5,25);
+//     print_r(" $key ---> $value |");
+
+//     if($value % 2 === 0){
+//         $masyvas1[$key] = $value;
+//     }else {
+//         $masyvas2[$key] = $value;
+//     }
+// }
+
+// echo '<br>';
+// echo '<br>';
+// echo '<pre>';
+// print_r($masyvas1);
+
+// echo '<br>';
+// echo '<br>';
+// echo '<pre>';
+// print_r($masyvas2);
+
+
+//g)
+
+// $arr = range(0, 29);
+// $naujas = [];
+
+// foreach($arr as $key => $value){
+//     $value = rand(5,25);
+//     print_r(" $key ---> $value |");
+
+//     if($key % 2 === 0 && $key >= 15){
+//         $naujas[$key] = $arr[$value = 0];
+//     }
+// }
+
+// echo '<br>';
+// echo '<br>';
+// echo '<pre>';
+// print_r($naujas);
+
+//h)
+
+// $arr = range(0, 29);
+// $min = [];
+// $minKey = 999;
+
+// foreach($arr as $key => $value){
+//     $value = rand(5,25);
+//     print_r(" $key ---> $value |");
+
+//     if($value >= 10 && $key < $minKey){
+//     $minKey = $key;
+//     $min[$key] = $arr[$value]; 
+//     }
+
+// }
+
+// echo '<br>';
+// echo '<br>';
+// echo '<pre>';
+// print_r($min);
+
+//i)
+
+// foreach(range(1,30) as $_ ){
+//     $masyvas[] = rand(5,25);
+// }
+
+// foreach($masyvas as $key => $value){
+//     if($key % 2 === 0){
+//         unset($masyvas[$key]);
+//     }
+// }
+
+// echo '<pre>';
+// print_r($masyvas);
+
+//3
+
+foreach(range('A', 'D') as $key => $value ){
+    $masyvas[] = $value;
 }
-echo '<br>';
-echo '<br>';
-echo $sum;
+
+
+
+echo '<pre>';
+print_r($masyvas);
