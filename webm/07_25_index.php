498b2daa8262ec0b4fd0b8385172fab84937fcbe
Karolis <karolisdarbinis2@gmail.com>
2023-07-25 09:33:47 +0300

    Delete

---
+++ webm/07_25_index.php
@@ -0,0 +1,72 @@
+<?php
+
+$animals = [];
+if (file_exists(__DIR__ . '/animals.json')) {
+    $animals = json_decode(file_get_contents(__DIR__ . '/animals.json'), 1);
+}
+
+//POST
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if ($_GET['action'] == 'delete') {
+        foreach ($animals as $key => $animal) {
+            if ($animal['id'] == $_GET['id']) {
+                unset($animals[$key]);
+                break;
+            }
+        }
+    }
+
+    if ($_GET['action'] == 'create') {
+        $animals[] = [
+            'name' => $_POST['name'],
+            'id' => uniqid(),
+        ];
+    }
+
+    file_put_contents(__DIR__ . '/animals.json', json_encode($animals));
+    header('Location: http://localhost/iguanos/015/');
+    die;
+}
+
+//GET
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZOO</title>
+</head>
+
+<body>
+    <h1>ZOO</h1>
+    <ul>
+        <?php if (!empty($animals)) : ?>
+            <?php foreach ($animals as $animal) : ?>
+                <li>
+                    <?= $animal['name'] ?>
+                    <form action="http://localhost/bitas/07_11_basics/webm/07_25_index.php?action=delete&id=<? $animal['id'] ?>" method="post">
+                        <button type="submit">Delete</button>
+                    </form>
+                </li>
+            <?php endforeach ?>
+        <?php else : ?>
+            <li>
+                No animals
+            </li>
+        <?php endif ?>
+    </ul>
+    <fieldset>
+        <legend>Add animal</legend>
+        <form action="http://localhost/bitas/07_11_basics/webm/07_25_index.php?action=create" method="post">
+            <input type="text" name="name">
+            <button type="submit">Add</button>
+        </form>
+    </fieldset>
+</body>
+
+</html>
