f15940d7ec81cf398ff77a4ea20ced68ac0b78f9
Karolis <karolisdarbinis2@gmail.com>
2023-07-25 20:02:05 +0300

    Colors

---
+++ colors/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+define('URL', 'http://localhost/bitas/07_11_basics/colors/');
---
+++ colors/index.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/bootstrap.php';
+$title = 'Colors - Home';
+require __DIR__ . '/top.php';
+?>
+<?php require __DIR__ . '/menu.php' ?>
+
+<div class="home">
+    <h1>Home</h1>
+</div>
+
+<?php
+require __DIR__ . '/bottom.php';
