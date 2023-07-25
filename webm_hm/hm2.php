5f5fda8bef1d17b95fe9eec14eb6fcb4dca0f158
Karolis <karolisdarbinis2@gmail.com>
2023-07-25 07:58:35 +0300

    Update

--- webm_hm/hm2.php
+++ webm_hm/hm2.php
@@ -1,7 +1,7 @@
 <!-- 2) Sukurti puslapį panašų į 1 uždavinį, tiktai antro linko su perduodamu kintamuoju nedarykite, o vietoj to padarykite, URL eilutėje ranka įvedus GET kintamąjį color su RGB spalvos kodu (pvz color=ff1234) puslapio fono spalva pasidarytų tokios spalvos. -->
 
 <?php
-
+$color = isset($_GET['color']) ? $_GET['color'] : '000000';
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
+<body style="background-color: #<?= $color ?>;">
 </body>
 </html>
--- webm_hm/hm3.php
+++ webm_hm/hm3.php
@@ -1,7 +1,7 @@
 <!-- 3) Perdarykite 2 uždavinį taip, kad spalvą būtų galimą įrašyti į laukelį ir ją išsiųsti mygtuku GET metodu formoje. -->
 
 <?php
-
+$color = isset($_GET['color']) ? $_GET['color'] : '000000';
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
+<body style="background-color: <?= $color ?>;">
+<form action="" method="get">
+    <input type="color" name="color">
+    <button type="submit">Submit</button>
+</form>
 </body>
 </html>
