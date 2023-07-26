200a3e05cf00365132e1b0cfe8167759b4ecf3ee
Karolis <karolisdarbinis2@gmail.com>
2023-07-26 19:24:04 +0300

    Update

---
+++ colors/list.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/bootstrap.php';
+$title = 'Colors - Colors List';
+require __DIR__ . '/top.php';
+?>
+<?php require __DIR__ . '/menu.php' ?>
+
+<div class="list">
+    <h1>List of colors</h1>
+    <?php $colors = json_decode(file_get_contents(__DIR__ . '/colors.json'), 1) ?>
+    <div class="colors-list">
+        <?php foreach ($colors as $color) : ?>
+            <div class="color-bin">
+                <div class="color" style="background-color: <?= $color['hex'] ?>;">
+                    <?= $color['name'] ?>
+                </div>
+                <a class="red" href="<?= URL ?>delete.php?id=<?= $color['id'] ?>">Delete</a>
+            </div>
+        <?php endforeach ?>
+    </div>
+</div>
+
+<?php
+require __DIR__ . '/bottom.php';
